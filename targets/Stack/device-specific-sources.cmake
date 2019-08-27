e9d0594bd6696f727851bb1b369459734142a009
david-perez <hallaplay835@gmail.com>
2019-08-27 18:25:08 +0200

    Extract device-specific sources definitions needed by the OS
    Each supported target must now have an device-specific-sources.cmake file defining them.

---
+++ targets/Stack/device-specific-sources.cmake
@@ -0,0 +1,4 @@
+set(DEVICE_SPECIFIC_SOURCES ${DEVICE_SPECIFIC_SOURCES}
+    targets/${BOARD}/source/neopixels.c
+    targets/${BOARD}/source/pwmout_api.c
+    targets/${BOARD}/source/temperature.c)
---
+++ targets/efm32hg_slstk3400a/device-specific-sources.cmake
@@ -0,0 +1,2 @@
+set(DEVICE_SPECIFIC_SOURCES ${DEVICE_SPECIFIC_SOURCES}
+    targets/${BOARD}/source/bsp_stk_leds.c)
